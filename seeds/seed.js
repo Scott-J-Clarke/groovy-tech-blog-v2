3d5c556a9edae209b18366bb128bce9c4bd2ce85
Vivid Quill <sjdoublec@gmail.com>
2023-09-20 08:40:43 -0500

    Added seed data (userData.json) to test User table in db.

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,3 +1,28 @@
+// Import Sequelize object:
 const sequelize = require('../config/connection');
-// Copied this from 14-Activity 24; not sure if Module Challenge 14 needs this:
-// const { User } = require('../models');
+// Import 'User' model, which represents the 'User' table in the database:
+const { User } = require('../models');
+
+// Import user data as array of objects, where each object is a user and their associated data (email, password, etc.):
+const userData = require('./userData.json');
+
+// Function that seeds 'user_db':
+const seedDatabase = async () => {
+    // Synchronizes 'schema.sql' with 'User' model. 
+    // 'sync' method creates creates tables in database based on models.
+    // '({ force: true })' drops any tables with the existing name and recreates them:
+    await sequelize.sync({ force: true });
+
+    // 'bulkCreate' is a method provided by Sequelize to add a lot of records to 'User' table:
+    await User.bulkCreate(userData, {
+        // This enables the execution of any defined 'hooks' for each user and return the created user objects:
+        individualHooks: true,
+        returning: true,
+    });
+    
+    // Exits Node.js with a status code of '0', indicating successful execution:
+    process.exit(0);
+};
+
+// Calls the 'seedDatabase' function to start the seeding process:
+seedDatabase();
