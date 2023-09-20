e6868fcf3eabe384292cae540788a106f029ec84
Vivid Quill <sjdoublec@gmail.com>
2023-09-20 09:34:31 -0500

    Added 'try-catch' syntax to 'seed.js' to help identify error.

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,20 +8,24 @@ const userData = require('./userData.json');
 
 // Function that seeds 'user_db':
 const seedDatabase = async () => {
-    // Synchronizes 'schema.sql' with 'User' model. 
-    // 'sync' method creates creates tables in database based on models.
-    // '({ force: true })' drops any tables with the existing name and recreates them:
-    await sequelize.sync({ force: true });
+    try {
+        // Synchronizes 'schema.sql' with 'User' model. 
+        // 'sync' method creates creates tables in database based on models.
+        // '({ force: true })' drops any tables with the existing name and recreates them:
+        await sequelize.sync({ force: true });
 
-    // 'bulkCreate' is a method provided by Sequelize to add a lot of records to 'User' table:
-    await User.bulkCreate(userData, {
-        // This enables the execution of any defined 'hooks' for each user and return the created user objects:
-        individualHooks: true,
-        returning: true,
-    });
-    
-    // Exits Node.js with a status code of '0', indicating successful execution:
-    process.exit(0);
+        // 'bulkCreate' is a method provided by Sequelize to add a lot of records to 'User' table:
+        await User.bulkCreate(userData, {
+            // This enables the execution of any defined 'hooks' for each user and return the created user objects:
+            individualHooks: true,
+            returning: true,
+        });
+        
+        // Exits Node.js with a status code of '0', indicating successful execution:
+        process.exit(0);
+        } catch (err) {
+            console.log(err);
+        }
 };
 
 // Calls the 'seedDatabase' function to start the seeding process:
