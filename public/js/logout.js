dbb0ef1306b71c83c8167816b85e93ef0284df36
Vivid Quill <sjdoublec@gmail.com>
2023-09-21 11:39:56 -0500

    Wrote 'public/logout.js' route.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,24 +1,18 @@
-// Function that handles the logout functionality.
-// When user clicks on 'logout' button (in 'main.handlebars'), this function will be triggered:
 const logout = async () => {
-    // Sends a POST request to '/api/users/logout' endpoint on the server.
-    // This api route is responsible for logging out the user.
-    // 'await' keyword waits for a response from the server:
     const response = await fetch('/api/users/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' }, 
     });
 
-    // If logout is successful, user is redirected to the '/login' page:
+    // If logout is successful, user is redirected to homepage ('/'):
     if (response.ok) {
-        document.location.replace('/login');
+        document.location.replace('/');
     } else {
         alert('Failed to log out.');
     }
 };
 
-// Line selects HTML element with '#logout' ID and attaches an event listener to it.
-// When the element is clicked, the logout function is executed:
 document
     .querySelector('#logout')
     .addEventListener('click', logout);
+    
