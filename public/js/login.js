c117c3ea8769520373ff18f449e239abfa327828
Vivid Quill <sjdoublec@gmail.com>
2023-09-21 10:55:42 -0500

    App starting to work. Have written 'login.js' route.

--- public/js/login.js
+++ public/js/login.js
@@ -1,35 +1,28 @@
-// This is a 'login form' handler function. When the form is submitted, the function is triggered:
 const loginFormHandler = async (event) => {
     event.preventDefault();
 
-    // Function retrieves values entered in the email and password input fields using 'document.querySelector.'
-    // Values are 'trimmed' to remove any leading or trailing whitespace:
     const email = document.querySelector('#email-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
 
-    // Checks if both 'email' and 'password' have been entered.
-    // If both values ('email' and 'password') exist, sends a POST request to '/api/users/login' using fetch() function:
     if (email && password) {
-        const response = await fetch('/api/users/login', { // Is '/api/users/login' route in the code somewhere?
+        const response = await fetch('/api/users/login', {
             method: 'POST',
-            // The request includes 'email' and 'password' as JSON data in the request body.
+            // The request includes 'email' and 'password' as JSON data in the request body:
             body: JSON.stringify({ email, password }),
-            // Request header specifies that 'Content-Type' is 'application/json.'
-            // The server knows to expect and parse the request body as JSON:
-            // (Different types of data can be sent in the request body, such as form data or plain text.)
+            // Request header tells server to expect and parse request body as JSON:
             headers: { 'Content-Type': 'application/json' },
         });
 
-        // If the response from the server is successful (status code '200'), user is sent to the homepage:
+        // If the server response is successful, user is sent to the dashboard:
         if (response.ok) {
-            document.location.replace('/');
+            document.location.replace('/dashboard');
         } else {
             alert('Failed to log in.');
         }
     }
 };
 
-// When the form is submitted, the 'loginFormHanlder' function is invoked:
+// When form is submitted, 'loginFormHanlder' function is called:
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
