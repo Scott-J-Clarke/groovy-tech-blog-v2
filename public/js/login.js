0f21c5c96e6050dd512dc4323162c2244f93f41a
Vivid Quill <sjdoublec@gmail.com>
2023-09-28 10:29:33 -0500

    login routes added to application.

--- public/js/login.js
+++ public/js/login.js
@@ -7,13 +7,12 @@ const loginFormHandler = async (event) => {
     if (email && password) {
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            // The request includes 'email' and 'password' as JSON data in the request body:
             body: JSON.stringify({ email, password }),
             // Request header tells server to expect and parse request body as JSON:
             headers: { 'Content-Type': 'application/json' },
         });
 
-        // If the server response is successful, user is sent to the dashboard:
+        // If login is correct, user is sent to the dashboard:
         if (response.ok) {
             document.location.replace('/dashboard');
         } else {
@@ -22,7 +21,6 @@ const loginFormHandler = async (event) => {
     }
 };
 
-// When form is submitted, 'loginFormHanlder' function is called:
 document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
